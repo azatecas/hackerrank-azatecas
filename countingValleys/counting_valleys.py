178627dbe7a95d5b41fcba826ced3d56db8a6993
Luis PM <lpm@pop-os.localdomain>
2020-10-28 17:22:39 -0400

    counting valleys solution

---
+++ countingValleys/counting_valleys.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    vcount = 0
+    level = 0
+
+    mem = {"U":1, "D":-1}
+
+    for item in path:
+        level += mem[item]
+        if level == 0 and item == "U":
+            vcount += 1
+    return vcount
+
+
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
